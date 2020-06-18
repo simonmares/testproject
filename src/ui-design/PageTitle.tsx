fa8bea1249d0a7333c50db60502c2837077c6f3d
Šimon Mareš <simon.mares@protonmail.com>
2020-06-18 12:58:12 +0200

    tweak: fix internal layout of page title

--- src/ui-design/PageTitle.tsx
+++ src/ui-design/PageTitle.tsx
@@ -4,20 +4,21 @@ import { Heading } from "src/ui-styles/structure";
 
 export function PageTitle(props: { children: React.ReactNode }) {
   return (
-    <Heading
-      css={{
-        color: "#EF476F",
-        marginTop: "16px",
-        background: "#ef476f12",
-        display: "inline-block",
-        padding: "8px 32px",
-        borderRadius: "4px",
-        transform: "skew(-0.01turn, 1deg)",
-        boxShadow: "1px 1px 1px",
-      }}
-      level={1}
-    >
-      {props.children}
+    <Heading level={1}>
+      <span
+        css={{
+          color: "#EF476F",
+          marginTop: "16px",
+          background: "#ef476f12",
+          display: "inline-block",
+          padding: "8px 32px",
+          borderRadius: "4px",
+          transform: "skew(-0.01turn, 1deg)",
+          boxShadow: "1px 1px 1px",
+        }}
+      >
+        {props.children}
+      </span>
     </Heading>
   );
 }
